0f0f49901cff0582682db0448064cd95a511669a
Tejas <tejaskhartude@gmail.com>
2021-09-13 00:39:18 +0530

    Added a new file, let's see

---
+++ app/src/main/java/com/tejas/genericstuff/seperate/Prototype.kt
@@ -0,0 +1,12 @@
+package com.tejas.genericstuff.seperate
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class Prototype: AppCompatActivity() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
