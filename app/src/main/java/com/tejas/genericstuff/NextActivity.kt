f4cd3566d4ed6e33544d31be9adad35b113f59fc
Tejas <tejaskhartude@gmail.com>
2021-09-13 00:33:43 +0530

    Added new Kotlin File NextActivity.kt

---
+++ app/src/main/java/com/tejas/genericstuff/NextActivity.kt
@@ -0,0 +1,12 @@
+package com.tejas.genericstuff
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class NextActivity: AppCompatActivity() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
