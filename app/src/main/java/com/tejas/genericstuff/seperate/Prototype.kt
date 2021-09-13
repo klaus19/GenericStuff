ea4159e39b7e5059cb080fe7255cf740e0fdc377
Tejas <tejaskhartude@gmail.com>
2021-09-13 10:36:18 +0530

    New Changes Have been made

--- app/src/main/java/com/tejas/genericstuff/seperate/Prototype.kt
+++ app/src/main/java/com/tejas/genericstuff/seperate/Prototype.kt
@@ -1,14 +1,66 @@
 package com.tejas.genericstuff.seperate
 
+
+import android.app.ProgressDialog
 import android.os.Bundle
+import android.widget.FrameLayout
+import android.widget.ImageButton
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.coordinatorlayout.widget.CoordinatorLayout
+import com.tejas.genericstuff.R
 
-class Prototype: AppCompatActivity() {
+abstract class Prototype: AppCompatActivity() {
 
+    lateinit var mTextViewScreenTitle: TextView
+    lateinit var mImageButtonBack: ImageButton
+    lateinit var mProgressDialog: ProgressDialog
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        
+        mProgressDialog = ProgressDialog(this)
+        mProgressDialog.setMessage("Loading")
+        mProgressDialog.setCancelable(false)
+        mProgressDialog.isIndeterminate = true
     }
+
+    override fun setContentView(layoutResID: Int) {
+        var coordinatorLayout: CoordinatorLayout = layoutInflater.inflate(R.layout.activity_base, null) as CoordinatorLayout
+        var activityContainer: FrameLayout = coordinatorLayout.findViewById(R.id.layout_container)
+        mTextViewScreenTitle = coordinatorLayout.findViewById(R.id.text_screen_title) as TextView
+        mImageButtonBack = coordinatorLayout.findViewById(R.id.image_back_button)
+
+        layoutInflater.inflate(layoutResID, activityContainer, true)
+
+        super.setContentView(coordinatorLayout)
+    }
+
+    fun setScreenTitle(resId: Int) {
+        mTextViewScreenTitle.text = getString(resId)
+    }
+
+    fun setScreenTitle(title: String) {
+        mTextViewScreenTitle.text = title
+    }
+
+    fun getBackButton(): ImageButton {
+        return mImageButtonBack;
+    }
+
+    fun showProgressDialog() {
+        if(!mProgressDialog.isShowing) {
+            mProgressDialog.show()
+        }
+    }
+
+    fun dismissProgressDialog() {
+        if (mProgressDialog.isShowing) {
+            mProgressDialog.dismiss()
+        }
+    }
+}
+
+
+
 }
